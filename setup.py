eb1b8a966ca62df25c307c6567d50a5d3b8b4075
priyanka1304 <priyankasingh131313@gmail.com>
2023-12-16 18:42:53 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help reccomend top 3 food items in swiggy home page.',
+    author='Priyanka',
+    license='',
+)
