63e7a3e63ffbcb72eddab05bdd299e72cb88dd3a
priyanka1304 <priyankasingh131313@gmail.com>
2023-12-16 20:30:40 +0530

    Author name is rectified

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='This project will help reccomend top 3 food items in swiggy home page.',
-    author='Priyanka',
+    author='Priyanka Singh',
     license='',
 )
